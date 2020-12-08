c08d5f7e1b383b57903e391adad2f81bbf87b42a
Spoolglans <jerry@jerryalmen.se>
2020-12-08 18:15:20 +0100

    Day 6 completed

--- Day6/day6.ps1
+++ Day6/day6.ps1
@@ -1,4 +1,4 @@
-$InputData = (Get-Content "input_test.txt" -Raw) -Split "`r`n`r`n"
+$InputData = Get-Content "$PSScriptRoot\input.txt" -Delimiter "`r`n`r`n"
 
 function Measure-Answares {
     param (
@@ -7,29 +7,20 @@ function Measure-Answares {
     )
 
     if (-not $PartTwo) {
-        return $Answares | % {$_.Replace("`r`n", "").ToCharArray() | Sort-Object | Get-Unique}   
+        ($Answares | % {$_.Replace("`r`n", "").ToCharArray() | Sort-Object | Get-Unique}).Count  
     } Else {
         foreach ($Set in $Answares) {
             $Rows = ($Set | Measure-Object -Word).Words
-            $UniqueAnswares = Measure-Answares $Set
+            $SetAns = $Set | % {$_.Replace("`r`n", "").ToCharArray() | Group-Object}
 
-            Write-Host "$People : $UniqueAnswares"
-            if ($People -ge $UniqueAnswares) {
-                # $UniqueAnswares
-            }
+            $AllYes += (($SetAns | % {$_.Count -eq $Rows}) -eq $true).Count
         }
-    }
-    
 
-}
+        $AllYes
+    }
+}   
 
 Write-Host "Part #1: " -NoNewline
-(Measure-Answares -Answares $InputData).Count
+Measure-Answares -Answares $InputData
+Write-Host "Part #2: " -NoNewline
 Measure-Answares -Answares $InputData -PartTwo
-
-for ($i = 0; $i -lt $InputData.Count; $i++) {
-    Write-Host $i ":" $InputData[$i]
-    Write-Host ""
-}
-
-Write-Host "Count:" $InputData.Count
